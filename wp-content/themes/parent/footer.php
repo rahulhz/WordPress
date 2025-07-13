8ec4f76ef93439eec46aec98b4dc9b0e70c785d4
rahulhz <rahulcskm07@gmail.com>
2025-07-13 11:44:44 +0530

    Created mulitple dynamic entry in the website

--- wp-content/themes/parent/footer.php
+++ wp-content/themes/parent/footer.php
@@ -12,6 +12,11 @@
             <div class="site-footer__col-two">
               <h3 class="headline headline--small">Explore</h3>
               <nav class="nav-list">
+                <!-- <?php
+                    wp_nav_menu(array(
+                        'theme_location' => 'footerLocationOne'
+                    ))
+               ?> -->
                 <ul>
                   <li><a href="#">About Us</a></li>
                   <li><a href="#">Programs</a></li>
@@ -24,7 +29,13 @@
             <div class="site-footer__col-three">
               <h3 class="headline headline--small">Learn</h3>
               <nav class="nav-list">
-                <ul>
+               <!-- <?php
+                    wp_nav_menu(array(
+                        'theme_location' => 'footerLocationTwo'
+                    ))
+               ?> -->
+              
+              <ul>
                   <li><a href="#">Legal</a></li>
                   <li><a href="#">Privacy</a></li>
                   <li><a href="#">Careers</a></li>
